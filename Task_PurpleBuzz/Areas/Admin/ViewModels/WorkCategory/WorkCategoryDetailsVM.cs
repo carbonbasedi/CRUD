6ccc0b6142f24785c5699b16ccc0e7c530d85d2d
carbonbasedi <carbonbasedai@gmail.com>
2023-06-13 04:07:09 +0400

    init

---
+++ Task_PurpleBuzz/Areas/Admin/ViewModels/WorkCategory/WorkCategoryDetailsVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_PurpleBuzz.Areas.Admin.ViewModels.WorkCategory
+{
+	public class WorkCategoryDetailsVM
+	{
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
